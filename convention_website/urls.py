16fef9fd262e507f5c7fa5c0e22681b94abc1306
Pranav Puri <pranavpuri.p@hotmail.com>
2016-09-17 10:53:25 +0530

    modified urls.py

--- convention_website/urls.py
+++ convention_website/urls.py
@@ -17,10 +17,12 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
+import layout.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('layout.urls')),
+    url(r'^$',layout.views.home, name='home')
 ]
 
 if settings.DEBUG:
