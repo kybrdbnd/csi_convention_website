6b1a73b08c6769772df884a23555292c4a9e9037
Pranav Puri <pranavpuri.p@hotmail.com>
2016-09-18 11:42:17 +0530

    modified navbar for mobile

--- static/static_dirs/js/init.js
+++ static/static_dirs/js/init.js
@@ -1,7 +1,11 @@
 (function($) {
     $(function() {
 
-        $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav({
+      menuWidth: 240, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
         $('.parallax').parallax();
         $('.scrollspy').scrollSpy();
         $('.modal-trigger').leanModal();
